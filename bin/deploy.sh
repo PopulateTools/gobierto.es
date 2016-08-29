f15913bf4dfdf399fa544c63e5dd67bf1db82621
Fernando Blat <ferblape@gmail.com>
2016-08-29 11:22:06 +0200

    Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf _site/
-JEKYLL_ENV=production jekyll build
+JEKYLL_ENV=production bundle exec jekyll build
 rsync -av --delete --no-perms -I -O _site/ populate02:/var/www/gobierto.es/
 rm -rf _site/
