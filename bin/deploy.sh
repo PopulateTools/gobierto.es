f29d550a4a381f1dd02155ae5d48ca9f5bc1782a
Fernando Blat <ferblape@gmail.com>
2016-12-23 11:05:27 +0100

    New deploy host

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,5 +2,5 @@
 
 rm -rf _site/
 JEKYLL_ENV=production bundle exec jekyll build
-rsync -av --delete --no-perms -I -O _site/ populate02:/var/www/gobierto.es/
+rsync -av --delete --no-perms -I -O _site/ gobierto01:/var/www/gobierto.es/
 rm -rf _site/
