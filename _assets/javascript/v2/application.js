eef427aca5342a316d92ef9f4c1bedb53fccdf12
Javier Arce <javierarce@gmail.com>
2017-10-14 00:07:50 +0200

    animations

--- _assets/javascript/v2/application.js
+++ _assets/javascript/v2/application.js
@@ -1,8 +1,27 @@
-window.onload = function () {
+function isScrolledIntoView(elem) {
+  var scrollPosition = window.scrollY;
+  var docViewBottom = scrollPosition + window.outerHeight;
+  var rect = elem.getBoundingClientRect();
+  var elemTop = rect.y + window.scrollY - 100;
+  var elemBottom = elemTop + rect.height - 300;
+  return !(scrollPosition < elemTop || scrollPosition > elemBottom )
+
+}
+
+var setupIntroCover = function () {
+  var cover = document.querySelectorAll('.js-cover')[0];
+
+  if (cover) {
+    cover.classList.add('Cover--intro');
+  }
+};
+
+var setupTooltips = function () {
   var tooltips = document.querySelectorAll('.js-tooltip');
+
   for (var i = 0; i < tooltips.length; i++) {
     var tooltip = tooltips[i];
-var p = tooltip.parentElement;
+    var p = tooltip.parentElement;
     var parent = tooltip.parentElement.querySelector('a');
 
     p.onmouseleave = function () {
@@ -13,4 +32,24 @@ var p = tooltip.parentElement;
       tooltip.classList.remove('Tooltip--hidden');
     }
   };
+
+};
+
+window.onscroll = function () {
+  var elements = document.querySelectorAll('.js-module-cover');
+
+  for (var i = 0; i < elements.length; i++) {
+    var el = elements[i];
+
+    if (isScrolledIntoView(el)) {
+      el.classList.add('animate');
+    } else {
+      el.classList.remove('animate');
+    }
+  }
+}
+
+window.onload = function () {
+  setupTooltips();
+  setupIntroCover();
 };
