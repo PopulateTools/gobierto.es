0311820068a33126349a7175ff43493b7f929039
Javier Arce <javierarce@gmail.com>
2017-10-22 12:37:57 +0200

    Removes cache first

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf _site/
+rm -rf .asset-cache _site
 JEKYLL_ENV=production bundle exec jekyll build
 rsync -av --delete --no-perms -I -O _site/ gobierto01:/var/www/gobierto.es/
 rm -rf _site/
